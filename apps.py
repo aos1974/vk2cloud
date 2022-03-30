aa4a3d6114da05d292d2f05e275aaccea6d5db81
Aleksandr Sysoev <aosysoev@yandex.ru>
2022-03-30 23:35:11 +0300

    socials.py

--- apps.py
+++ apps.py
@@ -1,10 +1,9 @@
 #
 # Модуль APPS.PY 
-# определение класса (объекта) типа "приложение"
+# определение базового класса "приложение"
 # 
 
 import configparser
-import argparse
 import os
 
 #
@@ -29,6 +28,8 @@ class Application:
         # загружаем параметры приложения из файла, если задано имя файла
         if len(inifile) > 0:
             self.load_ini(inifile)
+        else:
+            self.update_config('STATUS', 'LOAD_INI', 'FAILED')
 
         return None
 
@@ -39,17 +40,40 @@ class Application:
         if os.path.isfile(inifile):
             # загружаем параметры из файла
             self.config.read(inifile)
+            self.update_config('STATUS', 'LOAD_INI', 'OK')
+        else:
+            self.update_config('STATUS', 'LOAD_INI', 'FAILED')
+            return False
 
         return True
     
-    # функция загрузки параметров приложения из командной строки
-    def load_params(self) -> bool:
+    # обновление конфигурационных параметров прилолжения
+    def update_config(self, section: str, setting: str, value: str) -> bool:
+
+        # проверяем наличии группы параметров конфигурации
+        if section not in self.config.sections():
+            # если такой груупы нет, тогда создаем ее с параметрами
+            self.config[section] = {setting: value}
+        else:
+            # проверяем наличие параметра в секции
+            if setting not in self.config.set:
+                # создаем параметр со значением
+                self.config.set[section] = {setting: value}
+            else:
+                # обновляем значение параметра
+                self.config.set(section, setting, value)
 
         return True
     
     # основная функция логики приложения 
-    def run(self) -> None:
+    def run(self) -> str:
 
-        return None
+        run_status = 'OK'
+
+        # проверяем статус загрузки ini-файла (параметров приложения)
+        if self.config['STATUS']['LOAD_INI'] != 'OK':
+            run_status = 'Ошибка загрузки INI-файла!'
+
+        return run_status
 
 # end class
--- snis.py
+++ snis.py
@@ -3,7 +3,9 @@
 # Основной модуль программы
 #
 
+import sys
 import apps
+import argparse
 
 #
 # Определение глобальных переменных и констант
@@ -16,16 +18,57 @@ INI_FILE = 'snis.ini'
 # Глобальные функции модуля
 #
 
+class snisApplication(apps.Application):
+
+    # основная функция логики приложения
+    def run(self, namespace: argparse.Namespace) -> str:
+
+        # вызываем метод run() родительского класса
+        run_status = super().run()
+        # проверяем результаты работы родительского метода
+        if run_status != 'OK':
+            return run_status
+        
+        # обновляем из параметров запуска конфигурацию программы
+        # --count, кол-во фотографий для загрузки
+
+        return run_status
+
+# end class snisApplication
+
+# функция инициализации объекта парсинга командной строки
+def createParser():
+
+    # создаем объект парсинга командной строки
+    parser = argparse.ArgumentParser()
+    # добавляем аргументы командной строки
+    # --file, имя ini-файла с параметрами программы (по умолчанию snis.ini)  
+    # --count, кол-во фотографий для загрузки
+    # --id, id (имя) пользователя соцсети
+    parser.add_argument('--file', type=str, default=INI_FILE, help='имя ini-файла с параметрами программы (по умолчанию snis.ini)')
+    parser.add_argument('--count', type=str, default=5, help='кол-во фотографий для загрузки (по умолчанию 5)')
+    parser.add_argument('--id', type=str, default='', help='id (имя) пользователя соцсети')
+
+    return parser
+
 #
 # Главная функция программы
 #
 
-def main():
+def main() -> None:
 
-    snisApp = apps.Application(INI_FILE)
-    snisApp.run()
-
-    print(snisApp.config['Main']['SocialNetwork'])
+    # создаем объект парсинга командной строки
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    
+    # инициализируем объект приложения программы
+    snisApp = snisApplication(namespace.file)
+    # запускаем основной обработчик приложения
+    result = snisApp.run(namespace)
+    if result == 'OK':
+        print('УСПЕХ: Резервная копия фотографий сделана!')
+    else:
+        print(f'ПРОБЛЕМА: {result}')
 
     return None
 #
